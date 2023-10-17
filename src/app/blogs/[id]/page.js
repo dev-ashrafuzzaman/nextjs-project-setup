63123cc36ee976be0eb483cbb522cb31058e731b
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-10-17 17:36:55 +0600

    [generateStaticParams]

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -1,20 +1,28 @@
+import LoadBloagData from '@/utils/LoadBloagData';
 import SingleBlogData from '@/utils/SingleBlogData';
 import React from 'react';
 
-export const generateMetadata = async ({params}) => {
-    const {title , body} =await SingleBlogData(params.id)
+export const generateMetadata = async ({ params }) => {
+    const { title, body } = await SingleBlogData(params.id)
     return {
         title: title,
         description: body
     }
 }
 
-const SingleBlogpage = async ({params}) => {
-    const {title , id} =await SingleBlogData(params.id)
+export const generateStaticParams = async () => {
+    const blogs = await LoadBloagData();
+
+    return blogs.map(({ id }) => ({
+        id: id.toString()
+    }))
+}
+const SingleBlogpage = async ({ params }) => {
+    const { title, id } = await SingleBlogData(params.id)
     return (
         <div>
             Single Blog {params.id}
-            {id}:{title} 
+            {id}:{title}
         </div>
     );
 };
